361540b7ae9fb945504abf25ccd2bc312c19afa6
Octavian Nita <mnothro@yahoo.com>
2013-12-26 23:23:52 +0200

    removed theme Read v3.6.1

--- wp-content/themes/read-v3-6-1/searchform.php
+++
@@ -1,9 +0,0 @@
-<form role="search" id="searchform" method="get" action="<?php echo home_url( '/' ); ?>">
-	<div>
-		<label class="screen-reader-text" for="s"><?php echo __( 'Search for:', 'read' ); ?></label>
-		
-		<input type="text" id="s" name="s" placeholder="<?php echo __( 'Enter keyword...', 'read' ); ?>" value="<?php the_search_query(); ?>" required="required">
-		
-		<input type="submit" id="searchsubmit" value="<?php echo __( 'Search', 'read' ); ?>">
-	</div>
-</form>
