aaf067c5ff50c60af4917171d50eb87438d3de78
Octavian Nita <mnothro@yahoo.com>
2014-03-08 21:23:23 +0100

    v. 2.0: final changes before release

--- wp-content/themes/read-v3-9-1/search.php
+++ wp-content/themes/read-v3-9-1/search.php
@@ -36,17 +36,10 @@
 													<!-- .entry-header -->
 													<!-- .entry-meta --> 
 													<footer class="entry-meta">
-														<?php echo __( 'posted in', 'read' ); ?>
-														<?php the_category( ', ' ); ?>
-														<?php echo __( 'on', 'read' ); ?> <a href="<?php the_permalink(); ?>" title="<?php the_time(); ?>" rel="bookmark"><time class="entry-date" datetime="2012-11-09T23:15:57+00:00"><?php echo get_the_date(); ?></time></a>
+														<a href="<?php the_permalink(); ?>" title="<?php the_time(); ?>" rel="bookmark"><time class="entry-date" datetime="2012-11-09T23:15:57+00:00"><?php echo get_the_date(); ?></time></a>
 														<span class="comments-link">
 															<?php comments_popup_link( __( '0 Comment', 'read' ), __( '1 Comment', 'read' ), __( '% Comments', 'read' ) ); ?>
 														</span>
-														<span class="by-author"> <?php echo __( 'by', 'read' ); ?>
-															<span class="author vcard">
-																<a class="url fn n" href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>" title="<?php echo __( 'View all posts by ', 'read' ) . get_the_author(); ?>" rel="author"><?php the_author(); ?></a>
-															</span>
-														</span>				
 													</footer>
 													<!-- .entry-meta -->
 												</article>
